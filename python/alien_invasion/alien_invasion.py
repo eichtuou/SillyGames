b14767817ccd713d7c35abb69898e694c636841a
eichtuou <22988667+eichtuou@users.noreply.github.com>
2023-01-28 23:48:12 -0500

    updated game

--- python/alien_invasion/alien_invasion.py
+++ python/alien_invasion/alien_invasion.py
@@ -1,3 +1,8 @@
+"""
+This is my version of the project from the Python Crash Course 3rd Edition Book.
+
+In Alien Invasion, the player controls a rocket ship that appears at the bottom center of the screen. The player can move the ship right and left using the arrow keys and shoot bullets using the spacebar. When the game begins, a fleet of aliens fills the sky and moves across and down the screen. The player shoots and destroys the aliens. If the player destroys all the aliens, a new fleet appears that moves faster than the previous fleet. If any alien hits the player’s ship or reaches the bottom of the screen, the player loses a ship. If the player loses three ships, the game ends.
+"""
 import sys
 import pygame
 from settings import Settings
@@ -10,63 +15,37 @@ class AlienInvasion:
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
+        pygame.display.set_caption("Alien Invasion")
+
+        # frame rate
+        self.clock = pygame.time.Clock()
 
         # import settings
         self.settings = Settings()
-        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-        self.settings.screen_width = self.screen.get_rect().width
-        self.settings.screen_height = self.screen.get_rect().height
-        pygame.display.set_caption("Alien Invasion")
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
 
-        # import ship
+        # ship
         self.ship = Ship(self)
 
 
     def run_game(self):
         """Start the main loop for the game."""
         while True:
-            self._check_events()
-            self.ship.update()
-            self._update_screen()
+            # watch for keyboard and mouse events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
 
+            # set frame rate to 60 FPS
+            self.clock.tick(60)
 
-    def _check_events(self):
-        """Respond to keypresses and mouse events."""
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-            elif event.type == pygame.KEYDOWN:
-                self._check_keydown_events(event)
-            elif event.type == pygame.KEYUP:
-                self._check_keyup_events(event)
-
-
-    def _check_keydown_events(self, event):
-        """Respond to keypresses."""
-        if event.key == pygame.K_RIGHT:
-            self.ship.moving_right = True
-        elif event.key == pygame.K_LEFT:
-            self.ship.moving_left = True
-        elif event.key == pygame.K_q:
-            sys.exit()
-
-
-    def _check_keyup_events(self, event):
-        """Respond to key releases."""
-        if event.key == pygame.K_RIGHT:
-            self.ship.moving_right = False
-        elif event.key == pygame.K_LEFT:
-            self.ship.moving_left = False
-
-
-    def _update_screen(self):
-        """Update images on the screen, and flip to the new screen."""
-        self.screen.fill(self.settings.bg_color)
-        self.ship.blitme()
-        pygame.display.flip()
+            # redraw the screen
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
+            pygame.display.flip()
 
 
 if __name__ == '__main__':
-    # Make a game instance, and run the game.
+    # make a game instance, and run the game.
     ai = AlienInvasion()
     ai.run_game()
