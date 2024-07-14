b177d6baf4ad3e67ab72abba9ab02658ee1bde55
eichtuou <22988667+eichtuou@users.noreply.github.com>
2024-07-13 20:26:08 -0400

    got p5.js library set up and added more basic/generic functionality to app.js

--- SnakeGame_NodeJS/app.js
+++ SnakeGame_NodeJS/app.js
@@ -1,12 +1,62 @@
+'use strict';
 var express = require('express');
-var app = express();
-var port = 1337;
+var path = require('path');
+var debug = require('debug')('appDebug');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 
-app.use(express.static('public'));
+// Import route handlers
+var routes = require('./routes/index');
+
+// Instantiate app
+var app = express();
+app.set('port', process.env.PORT || 1337);
+
+// Set up view engine
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.listen(port);
-app.get('/', function (req, res) {
-    res.render('index');
+// Set up middleware
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', routes);
+app.use('/p5', express.static(path.join(__dirname, 'node_modules/p5/lib')));
+
+// Catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// Development error handler (with stacktrace)
+if (app.get('env') === 'development') {
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// Production error handler (no stacktrace)
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+// Start server
+var server = app.listen(app.get('port'), function () {
+    debug('Server listening on port ' + server.address().port);
 });
 
